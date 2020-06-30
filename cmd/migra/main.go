8d72f59cffd4175082b3dea60443429a6b3bfe85
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2020-06-30 15:40:09 +0200

    minor changes

--- cmd/migra/main.go
+++ cmd/migra/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/lorciv/sbdioi40"
 )
@@ -16,11 +18,19 @@ var srcAddr = flag.String("src", "", "Address to the source OpenStack platform (
 var dstAddr = flag.String("dst", "", "Address to the destination OpenStack platform (authentication endpoint)")
 var username = flag.String("user", "", "Username for authentication on both platforms")
 var password = flag.String("pass", "", "Password for authentication on both platforms")
-var appname = flag.String("app", "", "Name of the application to migrate")
 var clean = flag.Bool("clean", true, "Remove temporary files from local storage after migration")
 
 func main() {
 	flag.Parse()
+
+	appname := flag.Arg(0)
+	if appname == "" {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] appname\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Migrate a SBDIOI40 application between the two given platforms.")
+		fmt.Fprintln(os.Stderr, "options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	srcPlat, err := sbdioi40.Connect(*srcAddr, *username, *password)
 	if err != nil {
@@ -32,7 +42,7 @@ func main() {
 	}
 	log.Print("connected successfully to both platforms")
 
-	snap, err := srcPlat.Snapshot(*appname)
+	snap, err := srcPlat.Snapshot(appname)
 	if err != nil {
 		log.Fatal(err)
 	}
